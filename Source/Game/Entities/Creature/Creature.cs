435773e476ed4c15aff445f549ac8f88526feacf
hondacrx <prelude_20032002@yahoo.com>
2023-01-20 09:10:35 -0500

    Fixes creature movement.

--- Source/Game/Entities/Creature/Creature.cs
+++ Source/Game/Entities/Creature/Creature.cs
@@ -1154,7 +1154,7 @@ namespace Game.Entities
             if (HasSpellFocus())
                 return true;
 
-            if (!HasUnitState(UnitState.Casting))
+            if (HasUnitState(UnitState.Casting))
                 return true;
 
             return false;
