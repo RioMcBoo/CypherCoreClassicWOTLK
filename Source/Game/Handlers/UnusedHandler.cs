a6e5af1710b239bad9978d9d687f35e212e8a98e
RioMcBoo <artem.jkl@gmail.com>
2023-02-12 22:35:27 +0200

    Add packet messages

--- Source/Game/Handlers/UnusedHandler.cs
+++ Source/Game/Handlers/UnusedHandler.cs
@@ -13,13 +13,13 @@ namespace Game
 {
     public partial class WorldSession
     {
-        [WorldPacketHandler(ClientOpcodes.BattlePayGetPurchaseList, Processing = PacketProcessing.Inplace)]
-        [WorldPacketHandler(ClientOpcodes.BattlePayGetProductList, Processing = PacketProcessing.Inplace)]
-        [WorldPacketHandler(ClientOpcodes.UpdateVasPurchaseStates, Processing = PacketProcessing.Inplace)]
-        [WorldPacketHandler(ClientOpcodes.Unknown_3743, Processing = PacketProcessing.Inplace)]
-        [WorldPacketHandler(ClientOpcodes.OverrideScreenFlash, Processing = PacketProcessing.Inplace)]
-        [WorldPacketHandler(ClientOpcodes.GetAccountCharacterList, Processing = PacketProcessing.Inplace)]
-        [WorldPacketHandler(ClientOpcodes.QueuedMessagesEnd, Processing = PacketProcessing.Inplace)]
+        //[WorldPacketHandler(ClientOpcodes.BattlePayGetPurchaseList, Processing = PacketProcessing.Inplace)]
+        //[WorldPacketHandler(ClientOpcodes.BattlePayGetProductList, Processing = PacketProcessing.Inplace)]
+        //[WorldPacketHandler(ClientOpcodes.UpdateVasPurchaseStates, Processing = PacketProcessing.Inplace)]
+        //[WorldPacketHandler(ClientOpcodes.Unknown_3743, Processing = PacketProcessing.Inplace)]
+        //[WorldPacketHandler(ClientOpcodes.OverrideScreenFlash, Processing = PacketProcessing.Inplace)]
+        //[WorldPacketHandler(ClientOpcodes.GetAccountCharacterList, Processing = PacketProcessing.Inplace)]
+        //[WorldPacketHandler(ClientOpcodes.QueuedMessagesEnd, Processing = PacketProcessing.Inplace)]
         void HandleUnused(EmptyPaket empty) { }        
     }
 }
