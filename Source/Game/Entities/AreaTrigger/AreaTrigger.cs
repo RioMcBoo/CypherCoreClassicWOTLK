51abd7a7bb4aabaf45d87f0362c19783caeb4b2e
hondacrx <prelude_20032002@yahoo.com>
2024-02-05 13:09:02 -0500

    Core/AreaTrigger: Fallback to CreateProperties script, if ScriptName of static areatrigger is not provided
    Port From (https://github.com/TrinityCore/TrinityCore/commit/7e061adffa20323a26c0a489ba70dd098689f6d7)

--- Source/Game/Entities/AreaTrigger/AreaTrigger.cs
+++ Source/Game/Entities/AreaTrigger/AreaTrigger.cs
@@ -788,7 +788,12 @@ namespace Game.Entities
         public int GetScriptId()
         {
             if (_spawnId != 0)
-                return Global.AreaTriggerDataStorage.GetAreaTriggerSpawn(_spawnId).ScriptId;
+            {
+                AreaTriggerSpawn spawn = Global.AreaTriggerDataStorage.GetAreaTriggerSpawn(_spawnId);
+                if (spawn != null && spawn.ScriptId != 0)
+                    return spawn.ScriptId;
+
+            }
 
             AreaTriggerCreateProperties createProperties = GetCreateProperties();
             if (createProperties != null)
