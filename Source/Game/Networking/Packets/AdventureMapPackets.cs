20a665965aeecd05fd2621e36f9c1eeb9972d107
hondacrx <prelude_20032002@yahoo.com>
2024-02-24 16:13:14 -0500

    Core/PacketIO: Implemented CMSG_CHECK_IS_ADVENTURE_MAP_POI_VALID and SMSG_PLAYER_IS_ADVENTURE_MAP_POI_VALID
    Port From (https://github.com/TrinityCore/TrinityCore/commit/f6646739908215311f110842ed41055a54dae1f0)

--- Source/Game/Networking/Packets/AdventureMapPackets.cs
+++ Source/Game/Networking/Packets/AdventureMapPackets.cs
@@ -1,8 +1,37 @@
 ﻿// Copyright (c) CypherCore <http://github.com/CypherCore> All rights reserved.
 // Licensed under the GNU GENERAL PUBLIC LICENSE. See LICENSE file in the project root for full license information.
 
+using Framework.Constants;
+
 namespace Game.Networking.Packets
 {
+    class CheckIsAdventureMapPoiValid : ClientPacket
+    {
+        public CheckIsAdventureMapPoiValid(WorldPacket packet) : base(packet) { }
+
+        public override void Read()
+        {
+            AdventureMapPoiID = _worldPacket.ReadInt32();
+        }
+
+        public int AdventureMapPoiID;
+    }
+
+    class PlayerIsAdventureMapPoiValid : ServerPacket
+    {
+        public PlayerIsAdventureMapPoiValid() : base(ServerOpcodes.PlayerIsAdventureMapPoiValid, ConnectionType.Instance) { }
+
+        public override void Write()
+        {
+            _worldPacket.WriteInt32(AdventureMapPoiID);
+            _worldPacket.WriteBit(IsVisible);
+            _worldPacket.FlushBits();
+        }
+
+        public int AdventureMapPoiID;
+        public bool IsVisible;
+    }
+
     class AdventureMapStartQuest : ClientPacket
     {
         public AdventureMapStartQuest(WorldPacket packet) : base(packet) { }
