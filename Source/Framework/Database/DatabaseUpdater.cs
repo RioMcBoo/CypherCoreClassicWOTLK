5cafefc05edaee263273f01bf50b153358c5b6ec
RioMcBoo <artem.jkl@gmail.com>
2024-03-29 15:55:06 +0200

    TDB 343.23121 - 2023/12/20

--- Source/Framework/Database/DatabaseUpdater.cs
+++ Source/Framework/Database/DatabaseUpdater.cs
@@ -37,10 +37,10 @@ namespace Framework.Database
                     fileName = @"/sql/base/characters_database.sql";
                     break;
                 case "WorldDatabase":
-                    fileName = @"/sql/TDB_full_world_1015.23071_2023_07_14.sql";
+                    fileName = @"/sql/TDB_full_world_343.23121_2023_12_20.sql";
                     break;
                 case "HotfixDatabase":
-                    fileName = @"/sql/TDB_full_hotfixes_1015.23071_2023_07_14.sql";
+                    fileName = @"/sql/TDB_full_hotfixes_343.23121_2023_12_20.sql";
                     break;
             }
 
