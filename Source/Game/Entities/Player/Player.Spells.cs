CypherCore/CypherCore

Fixes alot of little errors.

Source/Game/Entities/Player/Player.Spells.cs
@@ -255,7 +255,7 @@ namespace Game.Entities
 
         public bool CanSeeSpellClickOn(Creature creature)
         {
-            if (creature.HasNpcFlag(NPCFlags.SpellClick))
+            if (!creature.HasNpcFlag(NPCFlags.SpellClick))
                 return false;
 
             var clickBounds = Global.ObjectMgr.GetSpellClickInfoMapBounds(creature.GetEntry());

RioMcBoo/CypherCoreClassicWOTLK

Source/Game/Entities/Player/Player.Spells.cs
@@ -260,7 +260,7 @@ namespace Game.Entities
 
         public bool CanSeeSpellClickOn(Creature creature)
         {
-            if (creature.HasNpcFlag(NPCFlags1.SpellClick))
+            if (!creature.HasNpcFlag(NPCFlags1.SpellClick))
                 return false;
 
             var clickBounds = Global.ObjectMgr.GetSpellClickInfoMapBounds(creature.GetEntry());