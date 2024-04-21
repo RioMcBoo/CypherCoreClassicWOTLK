CypherCore/CypherCore

Core/Creatures: Implemented UNIT_NPC_FLAG_GOSSIP as viewer dependent and update npcflags on quest changes
Port From (https://github.com/TrinityCore/TrinityCore/commit/dd15d763cc0529afc9b7f077817645d875608626)

Source/Game/Entities/Object/Update/UpdateFields.cs
@@ -2332,8 +2332,18 @@ namespace Game.Entities
         uint GetViewerDependentNpcFlags(UnitData unitData, int i, Unit unit, Player receiver)
         {
             uint npcFlag = unitData.NpcFlags[i];
-            if (i == 0 && unit.IsCreature() && !receiver.CanSeeSpellClickOn(unit.ToCreature()))
-                npcFlag &= ~(uint)NPCFlags.SpellClick;
+            if (i == 0)
+            {
+                Creature creature = unit.ToCreature();
+                if (creature != null)
+                {
+                    if (!receiver.CanSeeGossipOn(creature))
+                        npcFlag &= ~(uint)(NPCFlags.Gossip | NPCFlags.QuestGiver);
+
+                    if (!receiver.CanSeeSpellClickOn(creature))
+                        npcFlag &= ~(uint)NPCFlags.SpellClick;
+                }
+            }
 
             return npcFlag;
         }

RioMcBoo/CypherCoreClassicWOTLK

Source/Game/Entities/Object/Update/UpdateFields.cs
@@ -1972,8 +1972,18 @@ namespace Game.Entities
         uint GetViewerDependentNpcFlags(UnitData unitData, int i, Unit unit, Player receiver)
         {
             uint npcFlag = unitData.NpcFlags[i];
-            if (i == 0 && unit.IsCreature() && !receiver.CanSeeSpellClickOn(unit.ToCreature()))
-                npcFlag &= ~(uint)NPCFlags1.SpellClick;
+            if (i == 0)
+            {
+                Creature creature = unit.ToCreature();
+                if (creature != null)
+                {
+                    if (!receiver.CanSeeGossipOn(creature))
+                        npcFlag &= ~(uint)(NPCFlags1.Gossip | NPCFlags1.QuestGiver);
+
+                    if (!receiver.CanSeeSpellClickOn(creature))
+                        npcFlag &= ~(uint)NPCFlags1.SpellClick;
+                }
+            }
 
             return npcFlag;
         }