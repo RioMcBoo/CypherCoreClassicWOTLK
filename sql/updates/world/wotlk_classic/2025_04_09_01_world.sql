2635708ef44b821c63eaad8b072ea7e5509ba6f6
LaurCzT <laur@lab-404.com>
2025-04-09 19:30:43 +0300

    Fix some creatures and added missing forges & anvils in Stormwind

---
+++ sql/updates/world/wotlk_classic/2025_04_09_01_world.sql
@@ -0,0 +1,8 @@
+
+-- Mising gameobejcts in Darwen District (forge & anvil) -- no need to delete by guid since guid is not present in db
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `StringId`, `VerifiedBuild`) VALUES 
+(13810, 23302, 0, 0, 0, -8421, 616.391, 95.455, -1, 0, 0, -0.479426, 0.877583, 120, 0, 1, '', NULL, 0),
+(13812, 23303, 0, 0, 0, -8425, 614.482, 95.245, 1.998, 0, 0, 0.84093, 0.541143, 120, 0, 1, '', NULL, 0),
+(13813, 23304, 0, 0, 0, -8421, 613.438, 95.181, -2, 0, 0, -0.841471, 0.540302, 120, 0, 1, '', NULL, 0),
+(13814, 23305, 0, 0, 0, -8425, 618.065, 95.387, -2, 0, 0, -0.841471, 0.540302, 120, 0, 1, '', NULL, 0),
+(13970, 24746, 0, 0, 0, -8427.56, 631.026, 95.7472, 2.28742, 0, 0, 0.910177, 0.414221, 25, 0, 1, '', NULL, 0);
